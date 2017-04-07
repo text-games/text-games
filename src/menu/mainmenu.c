9265e22900f817ec9bd0d176a4b3885da287c35e
nicksms <nicksms@users.noreply.github.com>
2017-04-06 21:26:10 -0400

    Main menu base code

---
+++ src/menu/mainmenu.c
@@ -0,0 +1,9 @@
+#include "mainmenu.h"
+typedef enum menuCursorPos {FIRST = 1, SECOND = 2, THIRD = 3, FOURTH = 4, FIFTH = 5} menucursorpos;
+
+menucursorpos mcp_Main = FIRST;
+
+void startMenu()
+{
+	;
+}
---
+++ src/menu/mainmenu.h
@@ -0,0 +1,3 @@
+#include <ncurses.h>
+void startMenu();
+/*More code will go here in the future. For now, this is a pretty boring thing.*/
